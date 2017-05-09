410dbf17ffff9e19b54ef8907d075f509f6e0a19
Alan Bandeira <alan.p.bandeira@gmail.com>
2017-05-09 06:57:33 -0300

    minor change

--- Tracker/ocv_app/app/image_component/position_model.py
+++ Tracker/ocv_app/app/image_component/position_model.py
@@ -7,11 +7,19 @@ class PositionModel(Object):
     def __init__(self, cent_radius, coords):
         super(PositionModel, self).__init__()
 
+        # img coords
+        # obj_feature_vector - sectors
+        # bkgd_feature_vector - sectors
+        # obj_dim = (x' - x, y'- y)
         # centroid_radius
         # number of sectors
         # (radius - sector) list
-        # feature_vector - sectors
-        # img coords
 
         # obj_hist
         # bkgd_hist
+
+        # llr
+        # bitmask
+    
+    def set_bitmask_map(self):
+        pass
