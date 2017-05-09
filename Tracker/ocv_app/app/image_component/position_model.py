9e082b1f60af475755a6667aef8694f3f1a5a432
Alan Bandeira <alan.p.bandeira@gmail.com>
2017-05-09 14:55:52 -0300

    minor changes

--- Tracker/ocv_app/app/image_component/position_model.py
+++ Tracker/ocv_app/app/image_component/position_model.py
@@ -4,15 +4,21 @@ import cv2
 class PositionModel(Object):
     """docstring"""
 
-    def __init__(self, cent_radius, coords):
+    def __init__(
+        self, mean_radius, coords, obj_features, bkgd_feature, sectors):
         super(PositionModel, self).__init__()
 
-        # img coords
-        # obj_feature_vector - sectors
-        # bkgd_feature_vector - sectors
-        # obj_dim = (x' - x, y'- y)
-        # centroid_radius
-        # number of sectors
+        self.obj_coords = coords
+        self.obj_features = obj_features
+        self.bkgd_features = bkgd_feature
+
+        self.obj_dim = (
+            abs(obj_coords[0][0], obj_coords[1][0])
+            abs(obj_coords[0][1], obj_coords[1][1])
+        )
+
+        self.mean_radius = mean_radius
+        self.sectors = sectors
         # (radius - sector) list
 
         # obj_hist
